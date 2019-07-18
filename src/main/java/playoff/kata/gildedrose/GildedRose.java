edc65c832df4b3e3da54596a06f2ec4539983023
Edgar <esb920@gmail.com>
2019-07-18 20:05:22 +0200

    Up&running + constants

--- src/main/java/playoff/kata/gildedrose/GildedRose.java
+++ src/main/java/playoff/kata/gildedrose/GildedRose.java
@@ -1,6 +1,15 @@
 package playoff.kata.gildedrose;
 
 class GildedRose {
+	
+	private static final String BRIE = "Aged Brie";
+	private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+	private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+	private static final int GOOD_QUALITY = 50;
+	private static final int GOOD_SELL_IN = 11;
+	private static final int NORMAL_SELL_IN = 6;
+	private static final int BAD_SELL_IN = 0;
+	
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -9,28 +18,22 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
+            if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (items[i].quality > 0 && !items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                	items[i].quality = items[i].quality - 1;
                 }
             } else {
+            	//need to increase backstage 3 times
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                        	items[i].quality = items[i].quality + 1;
                         }
 
                         if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
+                        	items[i].quality = items[i].quality + 1;
                         }
                     }
                 }
@@ -57,6 +60,6 @@ class GildedRose {
                     }
                 }
             }
-        }
+        } //end for
     }
 }
